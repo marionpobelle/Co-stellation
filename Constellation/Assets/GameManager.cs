79949b5bfd9ed0f8f5be6b2d0ae4cb32218b80ed
Louis <>
2023-09-29 09:58:58 +0200

    Fred audio fixes

--- Constellation/Assets/GameManager.cs
+++ Constellation/Assets/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FindObjectOfType<AudioManager>().PlayOneShot("Music");
+        FindObjectOfType<AudioManager>().Play("Music");
 
     }
 
